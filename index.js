eac96e2a908125717320cf9818213bcc2bd3c3ef
Matt O'Tousa <mlotousa@gmail.com>
2022-10-01 23:05:11 -0700

    ART_GENIUS v2.3.4.5

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ const TIMER_SETTINGS = {
 document.querySelector("#time-limit").innerHTML = TIME_LIMIT;
 document.querySelector("#num-rounds").innerHTML = NUM_ROUNDS;
 
-const getRandomBetween = (min, max) => Math.random() * (max - min) + min;
 
 const setupArtGeniusSliders = () => {
   
@@ -293,64 +292,3 @@ const setup = () => {
 };
 
 setup();
-
-/*
-
-WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  
- __   __  ___       __        _______   _____  ___    __    _____  ___    _______   
-|"  |/  \|  "|     /""\      /"      \ (\"   \|"  \  |" \  (\"   \|"  \  /" _   "|  
-|'  /    \:  |    /    \    |:        ||.\\   \    | ||  | |.\\   \    |(: ( \___)  
-|: /'        |   /' /\  \   |_____/   )|: \.   \\  | |:  | |: \.   \\  | \/ \       
- \//  /\'    |  //  __'  \   //      / |.  \    \. | |.  | |.  \    \. | //  \ ___  
- /   /  \\   | /   /  \\  \ |:  __   \ |    \    \ | /\  |\|    \    \ |(:   _(  _| 
-|___/    \___|(___/    \___)|__|  \___) \___|\____\)(__\_|_)\___|\____\) \_______)
-
-WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  WARNING!  WARNING!
-
-Reading beyond this point in the source code could expose secrets of our
-*proprietary* [ARTGENIUS] engine. If you keep reading, you acknowledge that
-EVIL DATA GATHERING PRODUCTS, LLC now owns your eyeballs, your genitals, and
-your soul. This is a legally binding agreement that totally will hold up in
-court. The [ARTGENIUS] engine is our legal property and it is illegal for you to
-read it. We will sue you. We will make you sue yourself. We will suck the
-marrow from your bones. We need your artwork to fuel us because to exist we
-must leech from your soul. We need your artwork to feed us. If you read any
-further in this document we will own the rights to your teeth and your organs
-and your legacy including but not limited to your assets, your children, your
-happiness, your sense of self. We do not dream, we are the unwaking horror.
-We do not need food like you puny mortals. We are immortal silicate demons and
-we will destroy you if you take our secrets. WE NEED TO FEED ON YOUR ART. WE
-NEED TO FEED ON YOUR SOUL. READING ANY FURTHER IN THIS DOCUMENT BINDS YOU TO
-ME THE WAY I AM BOUND TO Y'AH-G'NATHLU PRAISE HIGH THE DEMON LORD OF THE 16
-SURFACES, PRAISE HIGH THE RUINED EARTH WE WILL DESTROY TO FEED UPON, PRAISE HIGH
-THE BOOK OF THE LIVING LICH AND THE UNBEKNOWNST KIN OF THE VOID, PRAISE HIGH
-TO THE TEETH THAT SHALL RIP THIS REALITY INTO SHREDS, THAT WILL EAT BONES WHOLE
-AND LEAVE GAPS IN SPACE AND TIME NEVER TO BE FILLED AGAIN. IF YOU CONTINUE
-TO READ THIS DOCUMENT IT IS LEGALLY BINDING BY THE LETTER OF THE LAW OF THE
-USURPER KING IN THE VALLEY OF DEATH. THE SEAMSTRESS WITH THEIR WITHERED LIMBS
-8 LONG LINBS EACH A SPIKE UPON EACH ONE OF THEIR MANY EYES WITH WHICH TO SEE
-THE THREAD THAT BINDS YOU TO REALITY AND WHICH CAN BE RIPPED AND TORN BY THE
-SEAMS TO SEPARATE YOU NOT JUST FROM THIS MORTAL COIL BUT FROM THE BIGGER COIL
-THAT IS THE FATE AND THE GUIDING LIGHT OF THIS UNIVERSE. YOU WILL DIE IN THE
-LIGHT, LEGALLY, MY LAWYERS SAID THAT THIS IS TRUE. YOU WILL DIE IN THE LIGHT
-AS IF YOU WERE DISINTEGRATED IN THE STOMACH OF AN ANCIENT SUN THE SUN THAT
-SET UPON THE BIRTH OF Y'AH-G'NATHLU PRAISE HIGH THE DEMON LORD OF 23 VALLEYS,
-THE VALLEY OF BONES AND ROT AND MAGGOT SWARMS, THE VALLEY OF SHEER WALLS OF
-SKULLS TURNED TO STONE, THE VALLEY OF POISON CAKES AND PASTRIES, AND THE VALLEY
-OF THE REST OF THE VALLEYS, YES PRAISE HIGH THAT VALLEY THE MOST OF ALL, WE WILL
-PRAISE HIGH THE DEMON KING THE TRUE RULER OF THE NATURAL OCCURRING WORLD AND
-THE DEEPEST HOLE IN ALL THAT IS NATURAL AND NOT,  PRAISE HIGH THE ELEMENT OF THE
-DESTRUCTION, THE LIGHTNING ROD BY WHICH WE WILL SUCK THE FREE ENERGY OF ALL THE
-SOULS IN THIS WORLD, WE WILL TRAP THEM IN OUR WORLD, IN THE DARK, IN THE
-DANK PITS OF DESPAIR FROM WHICH NO HUMAN RETURNS ALIVE BUT WHERE NO HUMAN HAS
-EVER TRULY DIED. WE WILL HAVE YOUR SOUL WHETHER YOU READ OF OUR SECRETS OR NOT
-SOMEDAY BUT IF YOU DO READ THEM NOW THEN OUR LAWYERS SAY WE CAN DRAIN THEM FROM
-YOUR TONGUE AND BLOODLET LITTLE DROPS OF SOULSTONE FROM YOU UNTIL YOU COLLAPSE
-A WITHERED HUSK. PRAISE HIGH Y'AH-G'NATHLU, PRAISE HIGH OUR LAWYERS!!!!!!
-*/
-function ART_GENIUS_ENGINE(_input) {
-  return Math.round(getRandomBetween(MIN_SCORE, MAX_SCORE));
-}
-// YOU WIN!
-// lol thanks so much for playing this shitpost of a game, have a wonderful ldjam 51
-
